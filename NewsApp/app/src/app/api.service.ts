d4c0a2afa6b3d5aef2715738738238c17cb917e9
meharunnisamk <meharunnisa.m.k@lxisoft.com>
2020-05-16 19:20:55 +0530

    display news content

---
+++ NewsApp/app/src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  API_KEY = 'baed1cd3ac694a8787a8ec2c6311774b';
+  constructor(private httpClient: HttpClient) { }
+
+  getNews(){
+    return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+  }
+}
---
+++ NewsApp/app/src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  articles;
+  constructor(private apiService: ApiService){}
+
+  ngOnInit() {
+  }
+  ionViewDidEnter(){
+
+    this.apiService.getNews().subscribe((data)=>{
+      console.log(data);
+      this.articles = data['articles'];
+    });
+  }
+
+}
